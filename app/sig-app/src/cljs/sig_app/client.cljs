b856065538af247710a12f62e0b7800424bec08a
Andrew Carlile <acarlile@sig-gis.com>
2025-08-19 11:44:44 -0400

    add vite conf from ceo

--- app/sig-app/src/cljs/sig_app/client.cljs
+++ app/sig-app/src/cljs/sig_app/client.cljs
@@ -1,5 +1,7 @@
 (ns sig-app.client
   (:require [reagent.dom :refer [render]]))
 
+(js/console.log "Hello World")
+
 (defn ^:export init [params session]
   (render [:div "Welcome to SIG!"] (js/document.getElementById "app")))
